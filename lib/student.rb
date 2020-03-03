9776ceda48f47928d57459bb01429f268b639860
lydzb123 <lydiabaik@gmail.com>
2020-03-02 19:20:45 -0800

    creates the students table in the database

--- lib/student.rb
+++ lib/student.rb
@@ -3,15 +3,24 @@ require_relative "../config/environment.rb"
 class Student
   attr_accessor :name, :grade
   attr_reader :id
-  
+
   def initialize(name, grade, id=nil)
     @name = name
     @grade = grade
     @id = id
   end
 
-  # def self.create_table
-  # end
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
+    );
+    SQL
+
+    DB[:conn].execute(sql)
+  end
 
   # def self.drop_table
   # end
