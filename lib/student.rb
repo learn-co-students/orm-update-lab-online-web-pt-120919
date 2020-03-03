9a35583e4df76f72bb6863a8f7b6e901df7c1a89
lydzb123 <lydiabaik@gmail.com>
2020-03-02 19:15:51 -0800

    initializes with name, grade, and id attributes

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,36 @@
 require_relative "../config/environment.rb"
 
 class Student
+  attr_accessor :name, :grade
+  attr_reader :id
+  
+  def initialize(name, grade, id=nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+
+  # def self.create_table
+  # end
+
+  # def self.drop_table
+  # end
+
+  # def save
+  # end
+
+  # def self.create
+  # end
+
+  # def self.new_from_db(arr)
+  # end
+
+  # def self.find_by_name
+  # end
+
+  # def update
+  # end
+
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
